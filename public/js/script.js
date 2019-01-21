a07d4bfbc14450316daf22d5ff14014ae605b7b7
Iryna Mazurenko <mazurenko.iryna@gmail.com>
2019-01-21 16:51:05 +0200

    Add movies-carusel

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
+  $(".owl-carousel.main-slider").owlCarousel({
   	items:1,
 		nav:true,
 		autoplay:true,
 		autoplayHoverPause:true,
 		nav:true
   });
+
+  $(".owl-carousel.item-slider").owlCarousel({
+		items:1,
+		nav:true,
+		autoplay:true,
+		autoplayHoverPause:true,
+		nav:true,
+		margin: 25,
+		responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  })
 });
 
 
